a54ac0662aa0e9341844e4fbe9e369cfe5392e14
NatasaMiric <natasa.miric94@gmail.com>
2023-04-19 12:51:03 +0000

    feat: Create Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    info = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_gucv3q', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+# this code was used from Code Institute drf-api walkthrough project
+# https://github.com/Code-Institute-Solutions/drf-api/blob/9ecb7addb100acb4223c6690b549edd4a9ed16a0/profiles/models.py
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+        
+
+post_save.connect(create_profile, sender=User)
