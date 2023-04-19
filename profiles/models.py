a5b04c57238610013f6a8102e2225829005a943b
NatasaMiric <natasa.miric94@gmail.com>
2023-04-19 14:01:33 +0000

    feat: Add profile detail view

--- profiles/models.py
+++ profiles/models.py
@@ -27,6 +27,6 @@ class Profile(models.Model):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
-        
+
 
 post_save.connect(create_profile, sender=User)
