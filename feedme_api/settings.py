de6769eee60c0c920a9a6eb3ac06e08f6589f883
NatasaMiric <natasa.miric94@gmail.com>
2023-04-19 17:12:39 +0000

    feat:Create recipes app and recipe model

--- feedme_api/settings.py
+++ feedme_api/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'profiles',
+    'recipes',
 ]
 
 SITE_ID = 1
---
+++ recipes/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Recipe(models.Model):
+    """
+    Recipe model, related to User instance.
+    Default image set so that we can always reference image.url.
+    """
+
+    CATEGORIES = [
+        ('appetizer', 'Appetizer'),
+        ('main course', 'Main course'),
+        ('dessert', 'Dessert'),
+        ('side dish', 'Side dish'),
+        ('beverage', 'Beverage'),
+        ('bread', 'Bread'),
+        ('snack', 'Snack'),
+        ('vegetarian', 'Vegetarian'),
+        ('condiments', 'Condiments'),
+        ('breakfast', 'Breakfast'),
+        ('luch', 'Lunch'),
+        ('dinner', 'Dinner')
+    ]
+
+    DIFFICULTY = [
+        ('easy', 'Easy'),
+        ('moderate', 'Moderate'),
+        ('hard', 'Hard')
+    ]
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    ingredients = models.TextField(blank=True)
+    instructions = models.TextField(blank=True)
+    category = models.CharField(max_length=25, choices=CATEGORIES, default='')
+    difficulty = models.CharField(
+        max_length=25, choices=DIFFICULTY, default=''
+        )
+    cooking_time = models.IntegerField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    recipe_image = models.ImageField(
+        upload_to='images/', default='../default_recipeimage_zn2eid',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
