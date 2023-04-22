9b8afddfc0ed52c5045240fcc3412aae3c28beb8
NatasaMiric <natasa.miric94@gmail.com>
2023-04-22 10:14:23 +0000

    test: Add Recipe List view tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Recipe
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class RecipeListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='john', password='pass')
+
+    def test_can_list_posts(self):
+        john = User.objects.get(username='john')
+        Recipe.objects.create(owner=john, title='a title')
+        response = self.client.get('/recipes/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_recipe(self):
+        self.client.login(username='john', password='pass')
+        response = self.client.post('/recipes/', {'title': 'a title'})
+        count = Recipe.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_user_not_logged_in_cant_create_recipe(self):
+        response = self.client.post('/recipes/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
