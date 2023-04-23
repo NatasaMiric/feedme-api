2cb41bb96ad23050de00fba11cafb29108a98959
NatasaMiric <natasa.miric94@gmail.com>
2023-04-23 10:26:41 +0000

    feat: Add Like List view to list all likes and create a like if logged in

--- likes/serializers.py
+++ likes/serializers.py
@@ -5,7 +5,7 @@ from likes.models import Like
 
 class LikeSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Like model.  
+    Serializer for the Like model.
     The create method handles the duplicate likes.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
