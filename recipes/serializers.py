a343514bc02fe3630ee10a2b5ccc4b5aa94488f7
NatasaMiric <natasa.miric94@gmail.com>
2023-04-23 12:44:10 +0000

    feat: Add like_id and bookmark_id to recipe serializer to know if current user already liked the recipe

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from recipes.models import Recipe
+from likes.models import Like
+from bookmarks.models import Bookmark
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -11,6 +13,8 @@ class RecipeSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    bookmark_id = serializers.SerializerMethodField()
 
     def validate_recipe_image(self, value):
         if value.size > 1024 * 1024 * 3:
@@ -31,11 +35,29 @@ class RecipeSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, recipe=obj
+            ).first()
+            return like.id if like else None
+        return None
+
+    def get_bookmark_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            bookmark = Bookmark.objects.filter(
+                owner=user, recipe=obj
+            ).first()
+            return bookmark.id if bookmark else None
+        return None
+
     class Meta:
         model = Recipe
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'created_at', 'updated_at', 'title', 'ingredients',
             'instructions', 'recipe_image', 'category', 'difficulty',
-            'cooking_time',
+            'cooking_time', 'like_id', 'bookmark_id'
         ]
