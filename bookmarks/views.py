db884011faa4f9faa7b024a571034ac672ed760c
NatasaMiric <natasa.miric94@gmail.com>
2023-04-23 11:48:14 +0000

    feat: Add bookmark detail view to retrieve and delete bookmark

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -5,14 +5,23 @@ from .serializers import BookmarkSerializer
 
 
 class BookmarkList(generics.ListCreateAPIView):
-    """ 
-    List all bookmarks. Create a bookmark if authenticated. 
-    The perform_create method associates the bookmark with the logged in user. 
+    """
+    List all bookmarks. Create a bookmark if authenticated.
+    The perform_create method associates the bookmark with the logged in user.
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = BookmarkSerializer
     queryset = Bookmark.objects.all()
-    
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+
+class BookmarkDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like. No Update view, as users can only save or unsave a recipe.
+    Destroy a like, i.e. unsave a post if owner of that save.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
