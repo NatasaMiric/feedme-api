17e4589269fd81c6871a08c98fd4ebc03b6e651f
NatasaMiric <natasa.miric94@gmail.com>
2023-04-24 11:50:26 +0000

    fix:Correct env vars for deployment

--- feedme_api/settings.py
+++ feedme_api/settings.py
@@ -170,7 +170,7 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
     }
 
 # Password validation
