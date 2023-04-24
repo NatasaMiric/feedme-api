ba12f6889196a9262a416a5a53e1ce76ff3d3257
NatasaMiric <natasa.miric94@gmail.com>
2023-04-24 11:36:14 +0000

    fix: Correct syntax in settings.py

--- feedme_api/settings.py
+++ feedme_api/settings.py
@@ -163,14 +163,14 @@ WSGI_APPLICATION = 'feedme_api.wsgi.application'
 
 if 'DEV' in os.environ:
     DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
-         }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
 else:
     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 # Password validation
